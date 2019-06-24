f66ca24370db6ea46dc7963975753d11014dadf2
Ecmel Berk Canlıer <me@ecmelberk.com>
2019-06-24 05:24:42 +0300

    Initial mod template

---
+++ src/main/java/club/issizler/template/TemplateMod.java
@@ -0,0 +1,22 @@
+package club.issizler.template;
+
+import club.issizler.okyanus.api.Mod;
+import club.issizler.okyanus.api.cmd.CommandBuilder;
+import club.issizler.okyanus.api.cmd.CommandManager;
+
+public class TemplateMod implements Mod {
+
+    @Override
+    public void init() {
+        System.out.println("Hello, world!");
+
+        CommandManager.INSTANCE.register(
+                new CommandBuilder()
+                        .name("hello")
+                        .run(source -> {
+                            source.send("Hello, world!");
+                            return 1;
+                        })
+        );
+    }
+}
